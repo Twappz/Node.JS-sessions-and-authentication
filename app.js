69d39d8c89f9c6970f3ece88021204c0936c04cb
unknown <rontab68@yahoo.com>
2021-07-16 06:29:56 -0700

    level 3

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt=require("mongoose-encryption");
+// const encrypt=require("mongoose-encryption");
+const md5 = require("md5");
+
 
 const app = express();
 
@@ -27,7 +29,7 @@ const userSchema = new mongoose.Schema ({
 
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:["password"]});
+// userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:["password"]});
 
 
 const User = new mongoose.model("User", userSchema);
@@ -46,7 +48,8 @@ app.get("/register", function (req, res) {
 app.post("/register", function (req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        // password: req.body.password
+        password:md5(req.body.password)
     });
 
     newUser.save(function (err) {
