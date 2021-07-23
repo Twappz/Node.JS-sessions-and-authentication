904c498dcc64a0dcf0ccfa912ecc8be5f79a2d64
unknown <rontab68@yahoo.com>
2021-07-22 18:53:33 -0700

    slated hash

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 // const encrypt=require("mongoose-encryption");
-const md5 = require("md5");
+// const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 
 
 const app = express();
@@ -23,7 +26,7 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
     useUnifiedTopology: true
 });
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
 
@@ -39,6 +42,7 @@ app.get("/", function (req, res) {
 
 app.get("/login", function (req, res) {
     res.render("login");
+
 });
 
 app.get("/register", function (req, res) {
@@ -46,23 +50,52 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", function (req, res) {
-    const newUser = new User({
-        email: req.body.username,
-        // password: req.body.password
-        password:md5(req.body.password)
+
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+
+        const newUser = new User({
+            email: req.body.username,
+            // password: req.body.password
+            // password:md5(req.body.password)
+            password: hash
+        });
+
+        newUser.save(function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        });
+
     });
 
-    newUser.save(function (err) {
+});
+
+app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function (err, foundUser) {
         if (err) {
             console.log(err);
         } else {
-            res.render("secrets");
+            if (foundUser) {
+
+                bcrypt.compare(password, foundUser.password,function(err, result){
+              if(result===true){
+                res.render("secrets");
+
+              }
+                });
+
+                
+            }
         }
+
     });
+
 });
-
-
-
 
 
 app.listen(3000, function () {
